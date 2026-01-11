2072fcd3ec76147c279f8333e0e73093ed738438
Muh Zayyan Al Thaaf Nur <aatzayyan49@gmail.com>
2026-01-11 19:12:17 +0700

    Membuat model dan migration order

---
+++ database/migrations/2026_01_11_121039_create_orders_table.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_code')->unique();
+            $table->integer('amount');
+            $table->string('status')->default('pending');
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
